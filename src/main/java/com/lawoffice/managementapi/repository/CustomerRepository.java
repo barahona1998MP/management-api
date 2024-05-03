d0fccd749e2e21251d2c93919c0debc7965c81af
cdquinterop <darmiqp@gmail.com>
2024-05-02 22:05:32 -0500

    cleaning CustomerRepository

--- src/main/java/com/lawoffice/managementapi/repository/CustomerRepository.java
+++ src/main/java/com/lawoffice/managementapi/repository/CustomerRepository.java
@@ -1,6 +1,5 @@
 package com.lawoffice.managementapi.repository;
 
-import com.lawoffice.managementapi.dto.CustomerDto;
 import com.lawoffice.managementapi.entity.Customer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
